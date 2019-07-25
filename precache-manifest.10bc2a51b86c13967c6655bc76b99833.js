6121bebe68b5877297a23146c2ab0ec584698d6b
Vicharm <vic010744@gmail.com>
2019-07-25 18:00:39 +0800

    Hello World

---
+++ precache-manifest.10bc2a51b86c13967c6655bc76b99833.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9083c9a3c2688b94ff65dbd7dbc531",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fda30fe3875b0c37d9b3",
+    "url": "/static/css/main.bed05e20.chunk.css"
+  },
+  {
+    "revision": "567c4ae4d078f9d858a2",
+    "url": "/static/js/2.dd88cc23.chunk.js"
+  },
+  {
+    "revision": "fda30fe3875b0c37d9b3",
+    "url": "/static/js/main.d3072d33.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6d00426556027e404f940355c6a142f3",
+    "url": "/static/media/album.6d004265.png"
+  },
+  {
+    "revision": "c86ec3d56b9ce6256f7b45bb08cecf08",
+    "url": "/static/media/balloon.c86ec3d5.mp3"
+  },
+  {
+    "revision": "be02c313a8b624c196ef259571b19c83",
+    "url": "/static/media/bar.be02c313.png"
+  },
+  {
+    "revision": "b9deb3c1e246c46bde24b81b1c859c9b",
+    "url": "/static/media/profile.b9deb3c1.png"
+  },
+  {
+    "revision": "9ba4dd8e6ecad1854405662c4dd0575b",
+    "url": "/static/media/score.9ba4dd8e.png"
+  }
+]);
